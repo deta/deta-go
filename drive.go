0620532b98fa292b82f25174cef741a27042e223
rohanshiva <rhnshiva@gmail.com>
2021-06-25 16:23:18 -0500

    drive sdk - list, delete, delete many

--- drive.go
+++ drive.go
@@ -1,10 +1,16 @@
 package deta
 
-
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"strings"
+)
+
+var (
+	ErrEmptyName    = errors.New("name is empty")
+	ErrEmptyNames   = errors.New("names is empty")
+	ErrTooManyNames = errors.New("more than 1000 names to delete")
 )
 
 type Drive struct {
@@ -31,9 +37,72 @@ func newDrive(projectKey, driveName, rootEndpoint string) *Drive {
 	}
 }
 
-// List file names from drive.
+type deleteRequest struct {
+	Names []string `json:names`
+}
+
+type deleteResponse struct {
+	Deleted []string          `json:deleted`
+	Failed  map[string]string `json:failed`
+}
+
+func (d *Drive) DeleteMany(names []string) (*deleteResponse, error) {
+	if names == nil {
+		return nil, ErrEmptyNames
+	}
+
+	if len(names) > 1000 {
+		return nil, ErrTooManyNames
+	}
+	o, err := d.client.request(&requestInput{
+		Path:   "/files",
+		Method: "DELETE",
+		Body: &deleteRequest{
+			Names: names,
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var dr deleteResponse
+	err = json.Unmarshal(o.Body, &dr)
+
+	if err != nil {
+		return nil, err
+	}
+	return &dr, nil
+}
+
+// Delete a file from the drive
 //
-func (d *Drive) list(limit int, prefix, last string) error {
+// If the file does not exist, a nil error is returned
+func (d *Drive) Delete(name string) error {
+	if name == "" {
+		return ErrEmptyName
+	}
+	payload := make([]string, 1)
+	payload = append(payload, name)
+	dr, err := d.DeleteMany(payload)
+	if err != nil {
+		return nil
+	}
+
+	_, ok := dr.Failed[name]
+	if !ok {
+		return fmt.Errorf("failed to delete %s", name)
+	}
+
+	return nil
+}
+
+type listResponse struct {
+	Paging *paging       `json:paging`
+	Names  []interface{} `json:names`
+}
+
+func (d *Drive) List(limit int, prefix, last string) (*listResponse, error) {
 	url := fmt.Sprintf("/files?limit=%d", limit)
 	if prefix != "" {
 		url = url + fmt.Sprintf("&prefix=%s", prefix)
@@ -41,6 +110,18 @@ func (d *Drive) list(limit int, prefix, last string) error {
 	if last != "" {
 		url = url + fmt.Sprintf("&last=%s", last)
 	}
-	fmt.Println(url)
-	return nil
+	o, err := d.client.request(&requestInput{
+		Path:   url,
+		Method: "GET",
+	})
+	if err != nil {
+		return nil, err
+	}
+	var lr listResponse
+	err = json.Unmarshal(o.Body, &lr)
+
+	if err != nil {
+		return nil, err
+	}
+	return &lr, nil
 }

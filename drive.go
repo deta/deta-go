80184075d3d5339f52aeab86c1bd814e9dc5bc09
rohanshiva <rhnshiva@gmail.com>
2021-07-01 10:31:01 -0500

    Delete, DeleteMany drive

--- drive.go
+++ drive.go
@@ -1,6 +1,7 @@
 package deta
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"strings"
@@ -45,4 +46,70 @@ func newDrive(projectKey, driveName, rootEndpoint string) *Drive {
 			headerValue: projectKey,
 		}),
 	}
+}
+
+type deleteManyRequest struct {
+	Names []string `json:"names"`
+}
+
+type DeleteManyOutput struct {
+	Deleted []string          `json:"deleted"`
+	Failed  map[string]string `json:"failed"`
+}
+
+// DeleteMany deletes multiple files in the Drive.
+//
+// Deletes at most 1000 files in a single request.
+// The files names should be in a slice.
+// Returns a response in the DeleteManyOutput interface format.  
+func (d *Drive) DeleteMany(names []string) (*DeleteManyOutput, error) {
+	if len(names) == 0 || names == nil {
+		return nil, ErrEmptyNames
+	}
+
+	if len(names) > 1000 {
+		return nil, ErrTooManyNames
+	}
+	o, err := d.client.request(&requestInput{
+		Path:   "/files",
+		Method: "DELETE",
+		Body: &deleteManyRequest{
+			Names: names,
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var dr DeleteManyOutput
+	err = json.Unmarshal(o.Body, &dr)
+
+	if err != nil {
+		return nil, err
+	}
+	return &dr, nil
+}
+
+// Delete a file from the drive
+//
+// If deleted (even if file with such name does not exist), returns the name
+// Else returns an error
+func (d *Drive) Delete(name string) (string, error) {
+	if name == "" {
+		return name, ErrEmptyName
+	}
+	payload := make([]string, 1)
+	payload = append(payload, name)
+	dr, err := d.DeleteMany(payload)
+	if err != nil {
+		return name, err
+	}
+
+	_, ok := dr.Failed[name]
+	if !ok {
+		return name, fmt.Errorf("failed to delete %s", name)
+	}
+
+	return name, nil
 }
